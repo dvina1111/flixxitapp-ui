c8bd1608d6f1ffde0e33b1886320d07424323bf4
dvina1111 <73433211+dvina1111@users.noreply.github.com>
2023-10-21 00:57:17 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://dvina1111:$reevarshaS_123@cluster0.42uv3pq.mongodb.net", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4 
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
